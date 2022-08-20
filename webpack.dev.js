096b171989f1a83ad599684b7990d965c1a2faad
Saul Vargas <saul.alberto.vargas@gmail.com>
2022-08-20 11:01:59 -0600

    Add files via upload

---
+++ webpack.dev.js
@@ -0,0 +1,98 @@
+const path = require("path");
+const fs = require("fs");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+//function to generate html files
+const generateHtmlPlugin = (args) => {
+	return new HtmlWebpackPlugin(args);
+};
+
+let entry = {};
+//loop through the pages folder and create a new html webpack plugin for each one
+let plugins = fs.readdirSync(path.resolve(__dirname, "./src/pages/")).map((file) => {
+	let name = file.split(".")[0];
+	entry[name] = `@ts/${name}.ts`;
+	return generateHtmlPlugin({
+		pageTitle: name == "index" ? "Login" : name,
+		pageName: name,
+		filename: file,
+		template: path.resolve(__dirname, `./src/pages/${file}`),
+		chunks: [name],
+	});
+});
+//push the global variable of the jquery library to the webpack bundle
+plugins.push(
+	new webpack.ProvidePlugin({
+		$: "jquery",
+		jQuery: "jquery",
+	})
+);
+//push the clean webpack plugin to the webpack plugins array
+plugins.push(new CleanWebpackPlugin());
+
+module.exports = {
+	mode: "development",
+	devServer: {
+		historyApiFallback: true,
+		static: path.resolve(__dirname, "./dist"),
+		open: true,
+		compress: true,
+		hot: true,
+		port: 8081,
+	},
+
+	//entry: path.resolve(__dirname, "./src/ts/index.ts"),
+	//entry: "@ts/index.ts",
+	entry: entry,
+
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "src/js/[name].js",
+	},
+
+	module: {
+		rules: [
+			// typescript
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+				type: "asset/resource",
+				generator: {
+					filename: "src/assets/images/[name].[ext]",
+				},
+			},
+			{
+				test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+				type: "asset/resource",
+				generator: {
+					filename: "src/assets/fonts/[name].[ext]",
+				},
+			},
+			{
+				test: /\.(sa|sc|c)ss$/,
+				use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+			},
+		],
+	},
+
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+		alias: {
+			"@ts": path.resolve(__dirname, "./src/ts/"),
+			"@utils": path.resolve(__dirname, "./src/utils/"),
+			"@assets": path.resolve(__dirname, "./src/assets/"),
+			"@pages": path.resolve(__dirname, "./src/pages/"),
+			"@components": path.resolve(__dirname, "./src/components/"),
+			"@sass": path.resolve(__dirname, "./src/assets/sass/"),
+			"@img": path.resolve(__dirname, "./src/assets/img/"),
+		},
+	},
+
+	plugins: plugins,
+};
---
+++ webpack.prod.js
@@ -0,0 +1,131 @@
+const webpack = require("webpack");
+const path = require("path");
+const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+//function to generate html files
+const generateHtmlPlugin = (args) => {
+	return new HtmlWebpackPlugin(args);
+};
+
+let entry = {};
+//loop through the pages folder and create a new html webpack plugin for each one
+let plugins = fs.readdirSync(path.resolve(__dirname, "./src/pages/")).map((file) => {
+	let name = file.split(".")[0];
+	entry[name] = `@ts/${name}.ts`;
+	return generateHtmlPlugin({
+		pageTitle: name == "index" ? "Login" : name,
+		pageName: name,
+		filename: file,
+		template: path.resolve(__dirname, `./src/pages/${file}`),
+		chunks: [name],
+	});
+});
+
+plugins.push(
+	new MiniCssExtractPlugin({
+		filename: "src/assets/css/[name].css",
+		chunkFilename: "src/assets/css/[name].chunk.css",
+	})
+);
+
+//push the global variable of the jquery library to the webpack bundle
+plugins.push(
+	new webpack.ProvidePlugin({
+		$: "jquery",
+		jQuery: "jquery",
+	})
+);
+//push the clean webpack plugin to the webpack plugins array
+plugins.push(new CleanWebpackPlugin());
+
+module.exports = {
+	mode: "production",
+
+	entry: entry,
+
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "src/js/[name].js",
+	},
+	/*optimization: {
+		runtimeChunk: "single",
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: "vendors",
+					chunks: "all",
+				},
+			},
+		},
+	},*/
+	//create one js file per vendor
+	optimization: {
+		runtimeChunk: "single",
+		splitChunks: {
+			chunks: "all",
+			maxInitialRequests: Infinity,
+			minSize: 0,
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name(module) {
+						// get the name. E.g. node_modules/packageName/not/this/part.js
+						// or node_modules/packageName
+						//if there is no node_modules skip.
+						if (module.context.indexOf("node_modules") === -1) return false;
+						const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+						// npm package names are URL-safe, but some servers don't like @ symbols
+						return `npm.${packageName.replace("@", "")}`;
+					},
+				},
+			},
+		},
+	},
+
+	module: {
+		rules: [
+			// JavaScript
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+				type: "asset/resource",
+				generator: {
+					filename: "src/assets/images/[name].[ext]",
+				},
+			},
+			{
+				test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+				type: "asset/resource",
+				generator: {
+					filename: "src/assets/fonts/[name].[ext]",
+				},
+			},
+			{
+				test: /\.(sa|sc|c)ss$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+			},
+		],
+	},
+
+	resolve: {
+		alias: {
+			"@ts": path.resolve(__dirname, "./src/ts/"),
+			"@utils": path.resolve(__dirname, "./src/utils/"),
+			"@assets": path.resolve(__dirname, "./src/assets/"),
+			"@pages": path.resolve(__dirname, "./src/pages/"),
+			"@components": path.resolve(__dirname, "./src/components/"),
+			"@sass": path.resolve(__dirname, "./src/assets/sass/"),
+			"@img": path.resolve(__dirname, "./src/assets/img/"),
+		},
+	},
+
+	plugins: plugins,
+};
